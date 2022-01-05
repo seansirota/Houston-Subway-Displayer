125f8ed0b0b899f48f6d6f0dc90a5d245f91acea
seansirota <95327356+seansirota@users.noreply.github.com>
2022-01-04 21:49:17 -0500

    Add files via upload

---
+++ MapWriter.ImplementationLibrary.HoustonSubway/LineEnums.cs
@@ -0,0 +1,45 @@
+﻿namespace HoustonSubway
+{
+    //This class contains the LineName enum which contains the enums for all valid Houston Subway lines.
+    public class LineEnums
+    {
+        public enum LineName
+        {
+            _,
+            North_Fwy, //A, K
+            Fulton_St, //A, B, K
+            Main_St, //A, B, K
+            Bellaire_Blvd, //A
+            Westheimer_Rd, //B
+            Cypresswood, //B, C
+            Stadium, //K
+            Sugarland, //C
+            Bellfort_Av, //C, N
+            Crawford_St, //C, F, N, R
+            Northern, //C, R
+            Airport, //G, M, R
+            Westpark_Tlwy, //F, R
+            Southwest_Fwy, //F, G, R
+            Market_St, //F, N
+            New_Forest, //N
+            Interbay, //F
+            Spring, //D, H, S
+            Broadway_St, //D
+            NASA, //D, T
+            Pasadena, //H, S
+            Baytown, //E, H, S
+            Tomball, //E
+            Shepherd_Dr, //E, L, Q, T
+            Pearland, //E, Q, T
+            Katy_Fwy, //Q
+            South_Pasadena, //E, T
+            South_Fwy, //M
+            Cullen_Blvd, //J
+            Eastern, //G, J, M
+            Lake_Houston, //J
+            Northwest_Fwy, //L
+            Atascocita, //L
+            Suburban //P
+        }
+    }
+}
---
+++ MapWriter.ImplementationLibrary.HoustonSubway/ServiceEnums.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HoustonSubway
+{
+    //This class contains the ServiceName enum which contains the enums for all valid Houston Subway services.
+    //It also contains the ServiceColor method definition, which assigns a ConsoleColor to each service.
+    public class ServiceEnums
+    {
+        public enum ServiceName
+        {
+            _,
+            A,
+            B,
+            C,
+            D,
+            E,
+            F,
+            G,
+            H,
+            J,
+            K,
+            L,
+            M,
+            N,
+            P,
+            Q,
+            R,
+            S,
+            T
+        }
+
+        public static ConsoleColor ServiceColor(ServiceName service)
+        {
+            List<ServiceName> greenList = new List<ServiceName> { ServiceName.A, ServiceName.B, ServiceName.K };
+            List<ServiceName> blueList = new List<ServiceName> { ServiceName.D, ServiceName.H, ServiceName.S };
+            List<ServiceName> yellowList = new List<ServiceName> { ServiceName.E, ServiceName.Q, ServiceName.T };
+            List<ServiceName> orangeList = new List<ServiceName> { ServiceName.C, ServiceName.F, ServiceName.N, ServiceName.R };
+            List<ServiceName> purpleList = new List<ServiceName> { ServiceName.G, ServiceName.J, ServiceName.M };
+            List<ServiceName> cyanList = new List<ServiceName> { ServiceName.L };
+            List<ServiceName> brownList = new List<ServiceName> { ServiceName.P };
+
+            if (greenList.Contains(service))
+                return ConsoleColor.Green;
+            else if (blueList.Contains(service))
+                return ConsoleColor.Blue;
+            else if (yellowList.Contains(service))
+                return ConsoleColor.Yellow;
+            else if (orangeList.Contains(service))
+                return ConsoleColor.DarkYellow;
+            else if (purpleList.Contains(service))
+                return ConsoleColor.DarkMagenta;
+            else if (cyanList.Contains(service))
+                return ConsoleColor.Cyan;
+            else if (brownList.Contains(service))
+                return ConsoleColor.DarkRed;
+            else
+                return ConsoleColor.White;
+        }
+    }
+}
